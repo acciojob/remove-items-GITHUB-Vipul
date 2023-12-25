b498ccc953a87c9c820fe85d6f9e6db6bd6b544b
GITHUB-Vipul <126069867+GITHUB-Vipul@users.noreply.github.com>
2023-12-25 15:50:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let submit=document.querySelector('form>input');
+submit.onclick=function()
+	{
+let selected=document.getElementById("colorSelect");
+for(let i=0;i<selected.length;i++)
+	{
+		if(selected.options[i].value==selected.value)
+		{
+			selected.remove(i);
+		}
+    }
+	};
